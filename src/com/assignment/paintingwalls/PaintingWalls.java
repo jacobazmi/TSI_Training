6413141db45845fd8b534884fc646c8865c36171
jacobazmi <jacobazmi@gmail.com>
2021-10-12 17:48:51 +0100

    added while loop to give option for multiple walls

--- src/com/assignment/paintingwalls/PaintingWalls.java
+++ src/com/assignment/paintingwalls/PaintingWalls.java
@@ -1,5 +1,6 @@
 package com.assignment.paintingwalls;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class PaintingWalls {
@@ -7,14 +8,40 @@ public class PaintingWalls {
 
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("How many walls do you want to paint?");
-        int walls = scan.nextInt();
+        //System.out.println("How many walls do you want to paint?");
+        //int walls = scan.nextInt();
 
-        System.out.println("How tall is the room in metres?");
-        double roomHeight = scan.nextDouble();
+        //System.out.println("How tall is the room in metres?");
+        //double roomHeight = scan.nextDouble();
 
-        System.out.println("How wide is the room in metres?"); // room assumed square
-        double roomWidth = scan.nextDouble();
+        //System.out.println("How wide is the room in metres?"); // room assumed square
+        //double roomWidth = scan.nextDouble();
+
+        double roomHeight = 0;
+        double roomWidth = 0;
+        int walls = 0;
+        String newWallAnswer = "yes";
+        double areaToPaint = 0;
+        double totalAreaToPaint = 0;
+
+
+        do {
+            System.out.println("How tall is the room in metres?");
+            roomHeight = scan.nextDouble();
+
+            System.out.println("How wide is the room in metres?"); // room assumed square
+            roomWidth = scan.nextDouble();
+
+            areaToPaint = roomHeight*roomWidth;
+            totalAreaToPaint = totalAreaToPaint + areaToPaint;
+
+            ++walls;
+
+            System.out.println("Would you like to add another wall?");
+            newWallAnswer = scan.next();
+
+        } while(Objects.equals(newWallAnswer, "yes"));
+
 
         System.out.println("What type of paint would you like to use? (flat/eggshell/satin/gloss");
         String paintType = scan.next();
@@ -42,13 +69,13 @@ public class PaintingWalls {
 
 
         double paintPotCoverage = 4.8*paintPotVolume;
-        double areaToPaint = roomHeight*roomWidth*walls;
-        double potsRequired = Math.ceil(areaToPaint/paintPotCoverage);
+        //double areaToPaint = roomHeight*roomWidth*walls;
+        double potsRequired = Math.ceil(totalAreaToPaint/paintPotCoverage);
         double paintUsed = areaToPaint/4.8;
         double remainingPaint = (potsRequired*paintPotVolume) - paintUsed;
         double totalCost = potsRequired*paintPotCost*costModifier;
 
-        System.out.println("To paint " + walls + " walls, the painter will need " + (int)potsRequired + " " + paintPotVolume + "L pot(s) of paint.");
+        System.out.println("To paint " + walls + " wall(s), the painter will need " + (int)potsRequired + " " + paintPotVolume + "L pot(s) of paint.");
         System.out.println("The total cost of the job will be " + "$" + String.format("%.2f",totalCost) + ".");
         System.out.println("There will be " + String.format("%.2f",remainingPaint) + " litres of paint leftover.");
 
